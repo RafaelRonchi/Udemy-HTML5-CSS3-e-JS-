e0e9dd95d4ecb9e7897918576321ba782ad29d8b
Rafael Eduardo Ronchi Filho <rafael.e01@aluno.ifsc.edu.br>
2023-03-07 18:09:04 -0300

    Terminando o form

--- Seções/27 Seção/FormLogin/script.js
+++ Seções/27 Seção/FormLogin/script.js
@@ -1,9 +1,18 @@
 (function(){
     'use strict';
+    
     var $form = document.querySelector('form')
     var $txtUser = document.getElementById('txtUser')
     var $txtPass = document.getElementById('txtPass')
     var $btn = document.getElementById('submit')
+    
+    /* $txtMsg.addEventListener('keypress', keypress)
+    $txtMsg.addEventListener('keyup', keyup)
+    $txtMsg.addEventListener('keydown', keydown) */
+
+    /* function keypress(e){}
+    function keyup(e){}
+    function keydown(e){} */
 
     $form.addEventListener('submit', function(){
         if(!$txtUser.value|| !$txtPass.value){
